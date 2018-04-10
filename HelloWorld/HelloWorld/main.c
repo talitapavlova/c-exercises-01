5370f44c21c4d8d35f0033b604a84b0eeb08a897
talitapavlova <talitapavlova@gmail.com>
2018-04-10 10:47:51 +0200

    Exercise 2

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,10 +1,22 @@
 
 #define F_CPU 10000000L
 #include <avr/io.h>
+#include <util/delay.h>
+
+#define led_no = 3;
 
 
-int main(void)
-{
+int main(void) {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(1<<PA0);      // turning on led 0 (active low)
+	PORTA = 0xff;
+	light_on(3);
+    
+}
+
+void light_on(int light_no) {
+	PORTA &= ~(1<<light_no);   
+}
+
+void light_off(int light_no) {
+	PORTA |= ~(1<<light_no); 
 }
