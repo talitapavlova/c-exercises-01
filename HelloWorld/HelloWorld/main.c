09cef694e8201aafc8fc6488c9865b03c21c696a
talitapavlova <talitapavlova@gmail.com>
2018-04-10 11:02:42 +0200

    Exercise 3

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -3,20 +3,34 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-#define led_no = 3;
+int led_no = 3;
+int light_state = 0;
 
+void light_on(int light_no);
+void light_off(int light_no);
 
 int main(void) {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
 	PORTA = 0xff;
-	light_on(3);
-    
+	
+	while(1) {
+		if(!light_state) {
+			light_on(led_no);
+			light_state = 1;
+		}
+		else {
+			light_off(led_no);
+			light_state = 0;
+		}
+	}
 }
 
 void light_on(int light_no) {
-	PORTA &= ~(1<<light_no);   
+	PORTA &= ~(1<<light_no);
+	_delay_ms(500);
 }
 
 void light_off(int light_no) {
-	PORTA |= ~(1<<light_no); 
+	PORTA |= 1<<light_no; 
+	_delay_ms(500);
 }
